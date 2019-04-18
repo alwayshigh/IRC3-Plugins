016f44d06d76588b809e3cfbe68016d0bccb4bcf
alwayshigh <alwayshigh@users.noreply.github.com>
2019-04-18 20:17:27 +1200

    Update currencyconverter.py
    Complete remake of script to use https://exchangeratesapi.io api since google finance and been deprecated.

--- currencyconverter/currencyconverter.py
+++ currencyconverter/currencyconverter.py
@@ -1,102 +1,96 @@
 import irc3
-import requests
-import re
-from bs4 import BeautifulSoup
 from irc3.plugins.command import command
+
+import re
+import requests
 
 
 @irc3.plugin
 class CurrencyConverterIRC3:
 
     def __init__(self, bot):
-        self.bot = bot
-        self.url = "https://finance.google.com/finance/converter"
-        self.currencies = self.availableCurrencies()
 
-    @command(options_first=True)
+        self.bot = bot
+        self.url = "https://api.exchangeratesapi.io/latest"
+
+        self.currencies = self.available_currencies()
+
+    @command
     def currency(self, mask, target, args):
         """Convert from one currency to another
 
-            %%currency <from> <to>
+           %%currency <from> <to>
         """
 
-        amount = 0
-        fromCurrency = None
-        toCurrency = None
+        base_amount = 0
+        currencies = {
+            "base": None,
+            "symbols": None
+        }
 
-        for c in self.currencies:
-            pattern = "^(-?[\d.]+){}$".format(c[0])
-            m = re.match(pattern, args["<from>"], re.IGNORECASE)
-            if m:
-                amount = m.group(1)
-                fromCurrency = str(c[0])
+        pattern = "^(\d.*\d)(\w+)$"
+        m = re.match(pattern, args["<from>"], re.IGNORECASE)
+        if m:
+            base_amount = m.group(1)
+            currencies["base"] = m.group(2).upper()
 
-            if args["<to>"].strip(" ").lower() == c[0]:
-                toCurrency = c[0]
-
-            if toCurrency and fromCurrency:
-                url = self.url + "?a={}&from={}&to={}".format(
-                    amount,
-                    fromCurrency,
-                    toCurrency
-                )
-                res = requests.get(url)
-                pattern = '.*<span class=bld>(.+)</span>.*'
-                total = re.search(pattern, res.text).group(1)
-                self.bot.privmsg(
-                    target,
-                    total
-                )
-                return
-
-        coins = []
-        if toCurrency is None:
-            coins.append(args["<to>"].upper())
-            
-        if fromCurrency is None:
-            coins.append(re.match("^-?[\d.]+(.+)$", args["<from>"]).group(1).upper())
-
-        
-        if len(coins) > 1:
-            output = "{} are invalid currencies."
-            output = output.format(" and ".join(coins))
+            if currencies["base"] not in self.currencies:
+                return "Please put both base and new currency"
         else:
-            output = "{} is an invalid currency".format(coins[0])
+            return "Please check you syntax"
 
-        self.bot.privmsg(
-            target,
-            output
+        if args["<to>"].upper() in self.currencies:
+            currencies["symbols"] = args["<to>"].upper()
+        else:
+            return "Please put both base and new currency"
+
+        data = self.request_api(params=currencies)
+        if not data:
+            return None
+
+        converted_amount = round(float(base_amount) * data["rates"][currencies["symbols"]], 2)
+
+        output = "{base_amount} {base_symbol} = {converted_amount} {converted_symbol}".format(
+            base_amount = base_amount,
+            base_symbol = currencies["base"],
+            converted_amount = converted_amount,
+            converted_symbol = currencies["symbols"]
         )
+
+        return output
 
     @command
     def currencylist(self, mask, target, args):
-        """Display all avaliable currencies IDs
+        """List avaiable currencies for conversion
 
-        %%currencylist
+           %%currencylist
         """
-        currencyList = ""
-        for c in self.currencies:
-            currencyList += c[0] + " "
-        
-        print(self.currencies)
+        return ", ".join(self.currencies)
 
-        self.bot.privmsg(
-            target,
-            currencyList
-        )
+    def available_currencies(self):
 
-
-    def availableCurrencies(self):
-        res = requests.get(self.url)
-        soup = BeautifulSoup(res.text, "html.parser")
-        options = soup.select('select[name=to] > option')
         currencies = []
-        for opt in options:
-            pattern = '.*<option value="(.+)">(.+?)\s\(.+</option>.*'
-            currency = re.match(pattern, str(opt))
 
-            currencies.append([
-                currency.group(1).lower(),
-                currency.group(2)
-            ])
+        data = self.request_api()
+        if not data:
+            return False
+
+        currencies.append(data["base"])
+
+        for currency in data["rates"].items():
+            currencies.append(currency[0])
+
         return currencies
+
+    def request_api(self, params={}):
+
+        try:
+            res = requests.get(self.url, params=params)
+            if res.status_code == 200:
+                return res.json()
+            else:
+                pass
+        except ConnectionError:
+            pass
+        print("Unable to connect to API")
+        return False
